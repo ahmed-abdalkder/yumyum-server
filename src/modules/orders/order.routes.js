25cffaf1fd62c9cf4a8abcefaea4030ce41c4d7b
Ahmed Abdulkadir <aabdelkder20@gmail.com>
2025-06-28 12:46:57 +0200

    url

--- src/modules/orders/order.routes.js
+++ src/modules/orders/order.routes.js
@@ -8,7 +8,7 @@ const router = Router();
 
 router.post("/", auth([systemroles.user]), OC.createOrder);
 router.put("/:id", auth([systemroles.user]), OC.CancelOrder);
-router.get("/AllOrder", auth([systemroles.user]), OC.getOrders);
+router.get("/AllOrder", auth([systemroles.user,systemroles.admin,systemroles.superadmin]), OC.getOrders);
 router.get("/Order/:id", auth([systemroles.user]), OC.getOrderById);
 router.get("/status/:id", auth([systemroles.user]), OC.getStatusOrder);
 
